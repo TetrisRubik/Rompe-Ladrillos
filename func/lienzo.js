5d364ef205627706d28da040b51af08aacbc545a
TetrisRubik <fpablo960@gmail.com>
2022-03-23 00:31:43 +0100

    Funcionalidad del lienza Se ha añadido el archivo javascript correspondiente al lienza para el trazado y renderizado del juego, así como el bucle de juego.

---
+++ func/lienzo.js
@@ -0,0 +1,25 @@
+import Juego from "./juego.js";
+
+let lienzo = document.getElementById("pantalla_juego");
+let ctx = lienzo.getContext("2d");
+
+const ancho_juego = 800;
+const alto_juego = 600;
+
+let juego = new Juego(ancho_juego, alto_juego);
+
+let ultima_tiempo = 0;
+
+function bucle_juego(tiempo_actual) {
+    let tiempo_delta = tiempo_actual - ultima_tiempo;
+    ultima_tiempo = tiempo_actual;
+
+    ctx.clearRect(0, 0, ancho_juego, alto_juego);
+
+    juego.actualiza(tiempo_delta);
+    juego.dibuja(ctx);
+
+    requestAnimationFrame(bucle_juego);
+}
+
+requestAnimationFrame(bucle_juego);
