026dcf625e16baf3a513a5b3bc66d5f58175c938
TetrisRubik <fpablo960@gmail.com>
2022-04-01 02:03:21 +0100

    Pelota comentada Unas adiciones finales a la pelota.

--- func/pelota.js
+++ func/pelota.js
@@ -1,3 +1,5 @@
+//* Objeto cinético que colisiona con todo.
+
 import { detecta_colision } from "./colisiones.js";
 
 export default class Pelota {
@@ -22,6 +24,7 @@ export default class Pelota {
 		this.tamaño = 20;
 	}
 
+	// Posicionar la pelota cuando se pierde.
 	reposicionamiento() {
 		this.posicion = {
 			x: this.ancho_juego / 2,
@@ -31,27 +34,31 @@ export default class Pelota {
 		this.velocidad.y = -3;
 	}
 
+	// Renderizado de la pelota.
 	dibuja(ctx) {
 		ctx.drawImage(this.imagen, this.posicion.x, this.posicion.y, this.tamaño, this.tamaño);
 	}
 
-	actualiza(tiempo_delta) {
+	// Movimiento de la pelota y detección de colisiones con el ambiente.
+	actualiza() {
 		this.posicion.x += this.velocidad.x;
 		this.posicion.y += this.velocidad.y;
 
-		// Colisiones horizontales y verticales
+		// Colisiones horizontales y verticales.
 		if (this.posicion.x + this.tamaño > this.ancho_juego || this.posicion.x < 0) {
 			this.velocidad.x = -this.velocidad.x;
 		}
 		if (this.posicion.y < 0) {
 			this.velocidad.y = -this.velocidad.y;
 		}
+
+		// La pelota se escapa.
 		if (this.posicion.y + this.tamaño > this.alto_juego) {
 			this.juego.vidas--;
 			this.reposicionamiento();
 		}
 
-		// Colisiones con paleta
+		// Colisiones con la paleta.
 		if (detecta_colision(this, this.juego.paleta)) {
 			this.velocidad.y = -this.velocidad.y;
 			this.posicion.y = this.juego.paleta.posicion.y - this.tamaño;
